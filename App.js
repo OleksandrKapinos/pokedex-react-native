8d68a7b0b481f4004bd404feb289bcff05d305e1
OleksandrKapinos <oleksandr.kapinos@gmail.com>
2020-05-13 03:12:37 +0300

    Start create list

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+  const urlImg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.text}>Pokedex</Text>
+      </View>
     </View>
   );
 }
@@ -13,7 +20,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    height: 80,
+    backgroundColor: '#2196F3',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 15
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Roboto',
+    fontSize: 25
+  }
 });
