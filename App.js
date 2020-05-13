7866bb3c8eb6dc4eb1d154414956b7be0d4fb112
OleksandrKapinos <oleksandr.kapinos@gmail.com>
2020-05-13 19:02:34 +0300

    have created list

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, Button, View, ScrollView } from 'react-native';
+import { PokemonList } from "./src/PokemonList";
 
 export default function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [pokemonQuantity, setpokemonQuantity] = useState(12);
+  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon';
 
-  const url = 'https://pokeapi.co/api/v2/pokemon';
-  const urlImg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full';
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.text}>Pokedex</Text>
-      </View>
-    </View>
-  );
+  useEffect(() => {
+    fetch(`${urlPokemon}?limit=${pokemonQuantity}`)
+        .then(response => response.json())
+        .then(list => {
+            setPokemons(list.results);
+            console.log(pokemons)
+        })
+  });
+
+  const addPokemon = (size) => {
+        setpokemonQuantity(size + 12);
+  };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.text}>Pokedex</Text>
+            </View>
+
+            <ScrollView>
+                {pokemons.length ? <PokemonList pokemons={pokemons}/> :
+                    <Text>No pokemons</Text>
+                }
+                <View style={styles.buttonBox}>
+                    <Button style={styles.button}
+                            title={'Add more'}
+                            onPress={() => addPokemon(pokemonQuantity)}
+                    /></View>
+            </ScrollView>
+
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -26,11 +53,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196F3',
     alignItems: 'center',
     justifyContent: 'flex-end',
-    padding: 15
+    padding: 10,
   },
   text: {
     color: '#fff',
     fontFamily: 'Roboto',
     fontSize: 25
-  }
+  },
+  button: {
+      backgroundColor: '#2196F3',
+      color: '#fff',
+  },
+    buttonBox: {
+        margin: 15,
+    }
 });
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
+import {PokemonCard} from './PokemonCard'
 
-export const PokemonList = () => {
+export const PokemonList = (prop) => {
     return (
-        <View></View>
+        <View style={styles.container}>
+            {prop.pokemons.map((pokemon, index) => {
+                console.log(index);
+                return <PokemonCard
+                    style={styles.card}
+                    name={pokemon.name}
+                    key={index}
+                    index={index + 1}
+                />
+            })}
+        </View>
     )
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center'
+    },
+    card: {
+        width: '50%',
+    }
 });
